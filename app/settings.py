e0b80821fb3f4180c9fb7cdf797a52f2e0fdc0d5
Lucas <vasconcelos.contabeis@gmail.com>
2025-02-23 10:56:59 -0300

    Adição do template base do projeto e do template de lista de carros.

--- app/settings.py
+++ app/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['app/templates'], # Adicionado o diretório de templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- app/urls.py
+++ app/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from cars.views import cars_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', cars_view, name='cars_list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
